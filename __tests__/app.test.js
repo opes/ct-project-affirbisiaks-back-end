926afe1bea71c9de9f192d8b06a0e87b52766a30
Isaac <isaac.ewing3@gmail.com>
2021-08-18 17:10:57 -0600

    put user route working and delete test made and failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,4 +28,30 @@ describe('back-end routes', () => {
     const res = await request(app).get(`/api/v1/users/${user.id}`);
     expect(res.body).toEqual({ id: 1, ...user.toJSON() });
   });
+  it('updates a user by id', async () => {
+    const user = await User.create({
+      username: 'name',
+      affirmations: [],
+      preference: '',
+      phoneNumber: ''
+    });
+    const newUser = {
+      username: 'newName',
+      affirmations: ['You\'re going to do great work today'],
+      preference: 'wholesome',
+      phoneNumber: '+19999999999'
+    };
+    const res = await request(app).patch(`/api/v1/users/${user.id}`).send(newUser);
+    expect(res.body).toEqual({ id: 1, ...newUser });
+  });
+  it('deletes a user by id', async () => {
+    const user = await User.create({
+      username: 'name',
+      affirmations: [],
+      preference: '',
+      phoneNumber: ''
+    });
+    const res = await request(app).delete(`/api/v1/users/${user.id}`);
+    expect(res.body).toEqual({ success: true });
+  });
 });
