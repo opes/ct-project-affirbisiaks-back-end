a6938018e355b5b1907f69c33caec3cdd2dd1864
Isaac <isaac.ewing3@gmail.com>
2021-08-18 16:20:47 -0600

    get user by id test failing because nothing else has been made

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,7 @@
 const database = require('../lib/utils/database');
 const request = require('supertest');
 const app = require('../lib/app');
+const User = require('../lib/models/User');
 
 describe('back-end routes', () => {
   beforeEach(() => {
@@ -16,5 +17,15 @@ describe('back-end routes', () => {
     };
     const res = await request(app).post('/api/v1/users').send(user);
     expect(res.body).toEqual({ id: 1, ...user });
+  });
+  it('gets a user by id', async () => {
+    const user = await User.create({
+      username: 'name',
+      affirmations: [],
+      preference: '',
+      phoneNumber: ''
+    });
+    const res = await request(app).get(`/api/v1/users/${user.id}`);
+    expect(res.body).toEqual({ ...user });
   });
 });
